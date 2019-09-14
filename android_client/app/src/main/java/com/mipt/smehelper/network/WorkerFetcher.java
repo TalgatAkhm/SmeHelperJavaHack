358157a262832f15986f4680988c877e2243fbe3
mike <mikelka150@gmail.com>
2019-09-15 01:56:30 +0300

    fetching users from us

--- android_client/app/src/main/java/com/mipt/smehelper/network/WorkerFetcher.java
+++ android_client/app/src/main/java/com/mipt/smehelper/network/WorkerFetcher.java
@@ -1,11 +1,16 @@
 package com.mipt.smehelper.network;
 
+import com.mipt.smehelper.EBMessages.UsersFetchedMessage;
 import com.mipt.smehelper.models.User;
+
+import org.greenrobot.eventbus.EventBus;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class WorkerFetcher {
+    private static ClientApiGet clientApiGet = NetworkService.getInstance().getClientApi();
+
     static List<User> fromAvito(User user) {
         String token = "cea83367c2231a09e8dce54b70c2dc91";
         List<User> users = new ArrayList<>();
@@ -13,9 +18,19 @@ public class WorkerFetcher {
         return users;
     }
 
-    static List<User> fromUs(User user) {
-        List<User> users = new ArrayList<>();
-
-        return users;
+    // Run in new thread, register event
+    static void fromUs(User user) {
+        new Thread() {
+            public void run() {
+                try {
+                    List<User> users = clientApiGet.getWorkers().execute().body();
+                    if (user != null) {
+                        EventBus.getDefault().post(new UsersFetchedMessage(users));
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
     }
 }

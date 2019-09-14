32967edda1202fe3d6aa89cd4c05f6af73065eaf
fflushlife <mikelka150@gmail.com>
2019-09-14 16:41:11 +0300

    retrofit added, first Transaction model

---
+++ android_client/app/src/main/java/com/mipt/smehelper/models/Transaction.java
@@ -0,0 +1,32 @@
+package com.mipt.smehelper.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.deser.std.NumberDeserializers;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Transaction {
+    @JsonProperty("isIncome")
+    private boolean isIncome;
+
+    @JsonProperty("amount")
+    private double amount;
+
+    @JsonProperty("purpose")
+    private String purpose;
+
+    @JsonProperty("realString")
+    private String realComment;
+
+    @JsonProperty("officialComment")
+    private String officialComment;
+
+    // May be remove se/deserialize
+    @JsonDeserialize(using = NumberDeserializers.NumberDeserializer.class)
+    @JsonSerialize(using = ToStringSerializer.class)
+    @JsonProperty("userId")
+    private Integer userId;
+}

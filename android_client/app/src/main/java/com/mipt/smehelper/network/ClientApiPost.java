ae7fa6450c8a639d7201b045a7045c8f3bdf031f
mike <mikelka150@gmail.com>
2019-09-14 19:12:58 +0300

    removed Transaction

--- android_client/app/src/main/java/com/mipt/smehelper/network/ClientApiPost.java
+++ android_client/app/src/main/java/com/mipt/smehelper/network/ClientApiPost.java
@@ -1,13 +1,5 @@
 package com.mipt.smehelper.network;
 
-import com.mipt.smehelper.models.Transaction;
-
-import okhttp3.ResponseBody;
-import retrofit2.Call;
-import retrofit2.http.Body;
-import retrofit2.http.POST;
-
 public interface ClientApiPost {
-    @POST("/send_transaction")
-    Call<ResponseBody> sendNewTransaction(@Body Transaction transaction);
+    
 }

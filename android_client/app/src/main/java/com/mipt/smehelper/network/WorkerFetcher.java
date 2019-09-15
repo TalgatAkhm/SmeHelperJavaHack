237487be195115a5a6088386c219aa9afa692465
mike <mikelka150@gmail.com>
2019-09-15 03:10:44 +0300

    avito fetching

--- android_client/app/src/main/java/com/mipt/smehelper/network/WorkerFetcher.java
+++ android_client/app/src/main/java/com/mipt/smehelper/network/WorkerFetcher.java
@@ -1,31 +1,89 @@
 package com.mipt.smehelper.network;
 
+import android.util.Log;
+
 import com.mipt.smehelper.EBMessages.UsersFetchedMessage;
 import com.mipt.smehelper.models.User;
 
 import org.greenrobot.eventbus.EventBus;
+import org.joda.time.DateTime;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class WorkerFetcher {
     private static ClientApiGet clientApiGet = NetworkService.getInstance().getClientApi();
 
-    static List<User> fromAvito(User user) {
-        String baseURL = "https://rest-app.net/api/ads?login=mikel1998@mail.ru&token=cea83367c2231a09e8dce54b70c2dc91&category_id=";
-        List<User> users = new ArrayList<>();
+    private final static String TAG = "WorkerFetcher";
+    private final static int READ_TIMEOUT_SEC = 30;
 
-        return users;
+    public static void fromAvito(int avitoId) {
+        new Thread() {
+            public void run() {
+                String baseURL = "https://rest-app.net/api/ads?login=mikel1998@mail.ru&token=cea83367c2231a09e8dce54b70c2dc91&category_id=" + String.valueOf(avitoId) + "&limit=10";
+                List<User> users = new ArrayList<>();
+                OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                        .readTimeout(READ_TIMEOUT_SEC, TimeUnit.SECONDS)
+                        .build();
+                Request.Builder builder = new Request.Builder();
+
+                builder = builder.url(baseURL);
+
+                Request request = builder.build();
+
+                Call call = okHttpClient.newCall(request);
+                try {
+                    Response response = call.execute();
+                    JSONArray values = new JSONObject(response.body().string()).getJSONArray("data");
+                    Log.d(TAG, "avito response is ok");
+
+                    for (int i = 0; i < values.length(); i++) {
+                        JSONObject object = values.getJSONObject(i);
+
+                        User user = new User();
+                        user.setSmallUSN(true);
+                        user.setRegDate(new DateTime().toString());
+                        user.setName(object.getString("name"));
+                        user.setJob(object.getString("title"));
+                        user.setCity(object.getString("city"));
+                        user.setAmount(Math.random() * 100000);
+
+                        users.add(user);
+                    }
+
+                    EventBus.getDefault().post(new UsersFetchedMessage(users));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        }.start();
     }
 
-    // Run in new thread, register event
-    static void fromUs(User user) {
+    // Register event
+    public static void fromUs(User user) {
         new Thread() {
             public void run() {
                 try {
                     List<User> users = clientApiGet.getWorkers().execute().body();
                     if (user != null) {
+                        Log.d(TAG, "users count fetched: " + String.valueOf(users.size()));
                         EventBus.getDefault().post(new UsersFetchedMessage(users));
+                    } else {
+                        Log.e(TAG, "users are null");
                     }
                 } catch (Exception e) {
                     e.printStackTrace();
